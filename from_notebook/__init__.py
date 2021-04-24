e46641b097918ed47f1a870324939a06b52bcbdb
Eli Sohl <wootfish@users.noreply.github.com>
2021-04-24 14:37:19 -0700

    Update __init__.py

--- from_notebook/__init__.py
+++ from_notebook/__init__.py
@@ -1,9 +1,3 @@
 from ._import_hook import install as _install
 
-
-# note: this does not do any mangling, so any notebook's filename will need to be changed to a valid python name
-# it also (probably) won't be able to handle notebooks named "_import_hook" or "_install"
-# (though if you have notebooks named shit like that, your issue is between you and your god)
-
-
 _install()
